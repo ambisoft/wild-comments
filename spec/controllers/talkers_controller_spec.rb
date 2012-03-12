cc0b0641e696738fef63329109e86fbf207af91a
ambisoft <mark@ambisoft.pl>
2012-03-12 01:42:36 +0100

    Rspec for the AJAX action returning the commenters

--- spec/controllers/talkers_controller_spec.rb
+++ spec/controllers/talkers_controller_spec.rb
@@ -2,18 +2,91 @@ require 'spec_helper'
 
 describe TalkersController do
 
-  describe "GET 'index'" do    
-    it "returns http success" do
-      
-      #@user = User.new(mock('graph'), 42)
-      #@graph = mock('graph')
-      #@friend = Friend.new(@graph, 1000, 'Tom')
-      
-      #@user.should_receive(:friend).with('1000').and_return(@friend)
-      
-      #get :index, :friend_id => @friend.uid
-      #response.should be_success
+  include OauthSpec
+  
+  describe "GET index" do
+    
+    before do
+      oauth_spec_init      
     end
+    
+    context 'when logged into facebook' do
+      
+      before do
+        oauth_user_login('1234567890', 42)        
+        
+        @friend = Friend.new(@graph, 1000, 'Tom')      
+        @user.should_receive(:friend).with(@friend.uid).and_return(@friend)      
+        
+      end
+    
+      it "returns http success" do      
+        
+        feed = []        
+        @graph.should_receive(:get_connections).with(@friend.uid, 'feed').and_return(feed)
+        
+        get :index, :friend_id => @friend.uid        
+        response.should be_success
+        
+      end
+      
+      it "returns commenters in JSON sorted by # of comments" do      
+        
+        user_john = { 'id' => 1, 'name' => 'John' }
+        user_tom = { 'id' => 2, 'name' => 'Tom' }
+      
+        feed = [
+          {
+            'id'  => 123,
+            'comments' => {
+              'count' => 2,
+              'data'  => [
+                { 'from' => user_john },
+                { 'from' => user_tom }
+              ]
+            }
+          },
+          {
+            'id'  => 124,
+            'comments' => {
+              'count' => 3,
+              'data'  => [
+                { 'from' => user_john },
+                { 'from' => user_john },
+                { 'from' => user_tom },
+              ]
+            }
+          }
+        ]
+                
+        feed_with_paging = FeedWithPaging.new(feed, feed.count)
+        
+        @graph.should_receive(:get_connections).with(@friend.uid, 'feed').and_return(feed_with_paging)
+        
+        get :index, :friend_id => @friend.uid, :format => :js
+        
+        expected =  '{"entries_count":0,"talkers":[' +
+                    '{"name":"John","uid":1,"count":3},' +
+                    '{"name":"Tom","uid":2,"count":2}' +
+                    '],"comments_count":0,"status":0}'
+                       
+        response.body.should == expected
+        
+      end
+      
+    end
+    
+    context 'when not logged into facebook' do
+      before do
+        @oauth.should_receive(:get_user_info_from_cookie).and_return(nil)
+        get :index, :friend_id => 1000
+      end
+
+      it 'should redirect to the login page' do
+        response.should redirect_to(:controller => :facebook, :action => :login)
+      end
+    end
+    
   end
 
 end
